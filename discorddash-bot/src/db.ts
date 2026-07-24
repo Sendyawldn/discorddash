5eef2e68ee0d5dd9a8beb4262bab9da265dedfc0
Sendyawldn <Sendyawaludin4568@gmail.com>
2026-07-24 14:00:07 +0700

    feat: integrate pg adapter for Prisma database connection

--- discorddash-bot/src/db.ts
+++ discorddash-bot/src/db.ts
@@ -1,11 +1,16 @@
-// Assuming we run from the bot directory, we import the Prisma Client generated in the web workspace
 import { PrismaClient } from '../../discorddash-web/node_modules/@prisma/client/index.js';
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 // Prevent multiple instances of Prisma Client in development
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
